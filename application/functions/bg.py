ad1b63adae0ab5953049b373b1fbeb657b38fdb1
本田　志温　 <26x.orc.ed5.1hs@gmail.com>
2018-09-06 17:36:02 +0900

    fix error

--- application/functions/bg.py
+++ application/functions/bg.py
@@ -14,7 +14,7 @@ You can change this as you like.
 class BG(object):
     def __init__(self, gpuid=-1, alpha=0.5, gamma=0.95, train=True, backprop=True):
         self.timing = brica.Timing(5, 1, 0)
-        self.agent = _set_agent(gpuid=gpuid)
+        self.agent = self._set_agent(gpuid=gpuid)
         self.reward = 0
         if gpuid<0:
             self.xp = numpy
@@ -55,40 +55,40 @@ class BG(object):
 
         return dict(to_pfc=None, to_fef=None, to_sc=self.xp.hstack((action, pfc_data)))
 
-def _phi(obs):
-    return obs.astype(self.xp.float32)
+    def _phi(self, obs):
+        return obs.astype(self.xp.float32)
 
-def _set_agent(gpuid=-1,actor_lr=1e-4, critic_lr=1e-3, gamma=0.995, minibatch_size=200):
-    q_func = chainerrl.q_functions.FCSAQFunction(
-        64, 64,
-        n_hidden_channels=16,
-        n_hidden_layers=3)
-    pi = chainerrl.policy.FCDeterministicPolicy(
-        64, action_size=64,
-        n_hidden_channels=16,
-        n_hidden_layers=3,
-        min_action=0, max_action=1,
-        bound_action=True)
-    if gpuid>=0:
-        q_func.to_gpu(gpuid)
-        pi.to_gpu(gpuid)
-    model = chainerrl.agents.ddpg.DDPGModel(q_func=q_func, policy=pi)
-    opt_a = optimizers.Adam(alpha=actor_lr)
-    opt_c = optimizers.Adam(alpha=critic_lr)
-    opt_a.setup(model['policy'])
-    opt_c.setup(model['q_function'])
-    opt_a.add_hook(chainer.optimizer.GradientClipping(1.0), 'hook_a')
-    opt_c.add_hook(chainer.optimizer.GradientClipping(1.0), 'hook_c')
-    rbuf = chainerrl.replay_buffer.ReplayBuffer(5 * 10 ** 5)
-    ou_sigma = 1 * 0.2 # Action space width
-    explorer = chainerrl.explorers.AdditiveOU(sigma=ou_sigma)
+    def _set_agent(self, gpuid=-1,actor_lr=1e-4, critic_lr=1e-3, gamma=0.995, minibatch_size=200):
+        q_func = chainerrl.q_functions.FCSAQFunction(
+            64, 64,
+            n_hidden_channels=16,
+            n_hidden_layers=3)
+        pi = chainerrl.policy.FCDeterministicPolicy(
+            64, action_size=64,
+            n_hidden_channels=16,
+            n_hidden_layers=3,
+            min_action=0, max_action=1,
+            bound_action=True)
+        if gpuid>=0:
+            q_func.to_gpu(gpuid)
+            pi.to_gpu(gpuid)
+        model = chainerrl.agents.ddpg.DDPGModel(q_func=q_func, policy=pi)
+        opt_a = optimizers.Adam(alpha=actor_lr)
+        opt_c = optimizers.Adam(alpha=critic_lr)
+        opt_a.setup(model['policy'])
+        opt_c.setup(model['q_function'])
+        opt_a.add_hook(chainer.optimizer.GradientClipping(1.0), 'hook_a')
+        opt_c.add_hook(chainer.optimizer.GradientClipping(1.0), 'hook_c')
+        rbuf = chainerrl.replay_buffer.ReplayBuffer(5 * 10 ** 5)
+        ou_sigma = 1 * 0.2 # Action space width
+        explorer = chainerrl.explorers.AdditiveOU(sigma=ou_sigma)
 
-    agent = chainerrl.agents.ddpg.DDPG(model, opt_a, opt_c, rbuf, gamma=gamma,
-                explorer=explorer, replay_start_size=5000,
-                target_update_method='soft',
-                target_update_interval=1,
-                update_interval=4,
-                soft_update_tau=1e-2,
-                n_times_update=1,
-                phi=_phi, gpu=-1, minibatch_size=minibatch_size)
-    return agent
+        agent = chainerrl.agents.ddpg.DDPG(model, opt_a, opt_c, rbuf, gamma=gamma,
+                    explorer=explorer, replay_start_size=5000,
+                    target_update_method='soft',
+                    target_update_interval=1,
+                    update_interval=4,
+                    soft_update_tau=1e-2,
+                    n_times_update=1,
+                    phi=self._phi, gpu=-1, minibatch_size=minibatch_size)
+        return agent
